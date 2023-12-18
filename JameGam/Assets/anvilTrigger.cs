cc724a52e9b10a1e3b6f2703a60012eda7efee24
Devels <109744785+kujs02029@users.noreply.github.com>
2023-12-18 11:33:35 -0600

    pixelArts1

---
+++ JameGam/Assets/anvilTrigger.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class anvilTrigger : MonoBehaviour
+{
+    [SerializeField] GameObject press;
+    public bool entered = false;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        press.SetActive(true);
+        entered = true;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        press.SetActive(false);
+        entered = false;
+    }
+}
