f97456e17a39fcd06ba9729df34f201b28ac4311
Unityiscool <jihoonmilk@gmail.com>
2023-12-18 09:07:59 -0600

    player movement

---
+++ JameGam/Assets/elfMovement.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows;
+using Input = UnityEngine.Input;
+
+public class elfMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float speed;
+    Vector2 input;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + input * speed * Time.fixedDeltaTime);
+    }
+
+    private void Update()
+    {
+        Vector2 mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = (mouseWorldPosition - (Vector2)transform.position).normalized;
+        transform.up = direction;
+        float v = Input.GetAxis("Vertical");
+        float h = Input.GetAxis("Horizontal");
+        input = new Vector2(h, v).normalized;
+    }
+}
