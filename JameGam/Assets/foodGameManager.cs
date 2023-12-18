e263871f3be3ba2b431f2a9c83d485a22527e5b4
Unityiscool <jihoonmilk@gmail.com>
2023-12-18 09:29:11 -0600

    food MG done

---
+++ JameGam/Assets/foodGameManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class foodGameManager : MonoBehaviour
+{
+    public List<GameObject> foods;
+    public List<GameObject> slots;
+    public List<string> types;
+    void Start()
+    {
+        for(int i = 0; i < 6; i++)
+        {
+            Instantiate(foods[Random.Range(0, foods.Count)], new Vector2(Random.Range(-8f, 8f), Random.Range(-4, -1)), Quaternion.identity);
+        }
+        
+        for (int i = 0;i < slots.Count; i++)
+        {
+            int a = Random.Range(0, types.Count);
+            slots[i].tag = types[a];
+            print(slots[i].tag);
+            types.RemoveAt(a);
+        }
+    }
+}
