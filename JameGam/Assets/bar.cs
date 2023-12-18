211de06cb0edf5bc5c8d2fb2d1dac5f1c694818e
Devels <109744785+kujs02029@users.noreply.github.com>
2023-12-18 09:43:49 -0600

    tree2

--- JameGam/Assets/bar.cs
+++ JameGam/Assets/bar.cs
@@ -7,6 +7,7 @@ public class bar : MonoBehaviour
     bool movement = true;
     public float speed = 5;
     bool stop = false;
+    [SerializeField] Rigidbody2D rb;
 
     // Update is called once per frame
     void FixedUpdate()
@@ -17,7 +18,7 @@ public class bar : MonoBehaviour
             {
                 if (gameObject.transform.position.y <= 3.9f)
                 {
-
+                    rb.MovePosition(rb.position + new Vector2(0, 1) * speed * Time.fixedDeltaTime);
                 }
                 else
                 {
@@ -28,7 +29,7 @@ public class bar : MonoBehaviour
             {
                 if (gameObject.transform.position.y >= -3.9f)
                 {
-
+                    rb.MovePosition(rb.position + new Vector2(0, -1) * speed * Time.fixedDeltaTime);
                 }
                 else
                 {
