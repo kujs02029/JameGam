1403a9650aea433bb3cd6964122a4867dfd17a8a
Unityiscool <jihoonmilk@gmail.com>
2023-12-19 10:10:45 -0600

    elf fixed

--- JameGam/Assets/ElfPoster.cs
+++ JameGam/Assets/ElfPoster.cs
@@ -4,13 +4,12 @@ using UnityEngine;
 
 public class ElfPoster : MonoBehaviour
 {
-    [SerializeField] Sprite[] elves;
     public int num;
     [SerializeField] SpriteRenderer renderer;
     // Start is called before the first frame update
     void Start()
     {
         num = Random.Range(0, 20);
-        renderer.sprite = elves[num];
+
     }
 }
