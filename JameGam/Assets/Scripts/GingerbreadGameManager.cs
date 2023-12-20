401e013100a216e844f412f643e230410229b718
Unityiscool <jihoonmilk@gmail.com>
2023-12-20 17:07:45 -0600

    gingerbread

--- JameGam/Assets/Scripts/GingerbreadGameManager.cs
+++ JameGam/Assets/Scripts/GingerbreadGameManager.cs
@@ -20,7 +20,7 @@ public class GingerbreadGameManager : MonoBehaviour
         while (true)
         {
             GameObject b = null;
-            int a = Random.Range(0, 6);
+            int a = Random.Range(0, 4);
             if (a == 0)
             {
                  b = Instantiate(gingerbread, new Vector3(Random.Range(-8f, 8f), 7), Quaternion.identity);
@@ -34,7 +34,6 @@ public class GingerbreadGameManager : MonoBehaviour
 
                 b = Instantiate(gingerbread, new Vector3(12, Random.Range(-4f, 4f)), Quaternion.identity);
             }
-                
             if (a == 3)
             {
                 b = Instantiate(gingerbread, new Vector3(-12, Random.Range(-4f, 4f)), Quaternion.identity);
