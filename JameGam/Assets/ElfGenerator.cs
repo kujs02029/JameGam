21a090cbdb2b08ba4a03d6d5c4d2f48c36e8222c
Unityiscool <jihoonmilk@gmail.com>
2023-12-21 17:32:56 -0600

    snow

---
+++ JameGam/Assets/ElfGenerator.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class ElfGenerator : MonoBehaviour
+{
+    public List<Sprite> elfTop;
+    public GameObject elf;
+    public int count;
+    [SerializeField] List<BoxCollider2D> bc;
+
+    Vector2 cubeSize;
+    Vector2 cubeCenter;
+
+
+    private void Awake()
+    {
+    }
+
+
+
+
+    private Vector2 GetRandomPosition()
+    {
+        // You can also take off half the bounds of the thing you want in the box, so it doesn't extend outside.
+        // Right now, the center of the prefab could be right on the extents of the box
+        Vector2 randomPosition = new Vector2(Random.Range(-cubeSize.x/2, cubeSize.x/2), Random.Range(-cubeSize.y/2 , cubeSize.y/2));
+
+        return cubeCenter + randomPosition;
+    }
+
+    void Start()
+    {
+        //make elf look at sleighs
+        for (int i = 0; i < count; i++)
+        {
+
+            int a = Random.Range(0, 8);
+            Transform cubeTrans = bc[a].GetComponent<Transform>();
+            cubeCenter = cubeTrans.position;
+            cubeSize.x = cubeTrans.localScale.x * bc[a].size.x;
+            cubeSize.y = cubeTrans.localScale.y * bc[a].size.y;
+            GameObject b = Instantiate(elf, GetRandomPosition(), Quaternion.identity);
+            b.GetComponent<SpriteRenderer>().sprite = elfTop[Random.Range(0, elfTop.Count)];
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
