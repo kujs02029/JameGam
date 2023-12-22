7db38ad490fc92d78b60715b3985a89223093711
Unityiscool <jihoonmilk@gmail.com>
2023-12-22 11:45:21 -0600

    script

--- JameGam/Assets/PlayerLapCounter.cs
+++ JameGam/Assets/PlayerLapCounter.cs
@@ -6,10 +6,16 @@ public class PlayerLapCounter : MonoBehaviour
 {
     int lap = 0;
     public int totalLap;
+    public bool touched;
+    private void Start()
+    {
+        touched = true;
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Finish"))
+        if (touched && collision.CompareTag("Finish"))
         {
+            touched = false;
             lap++;
             print(lap + "/" + totalLap);
         }
